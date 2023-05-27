38127eb26bf7f460fed311a83a91566336d14a68
crownVic0285 <vpcorona85@gmail.com>
2023-05-27 16:22:14 -0700

    class garage

--- car challenge.js
+++ car challenge.js
@@ -26,3 +26,19 @@ class Vehicle {
             return "VROOM!!!";
         }
     }
+    class Garage {
+        constructor(capacity) {
+            this.vehicles = [];
+            this.capacity = capacity;
+        }
+        add(newVehicle) {
+            if (!(newVehicle instanceof Vehicle)) {
+                return "Only vehicles are allowed in here!";
+            }
+            if (this.vehicles.length >= this.capacity) {
+                return "Sorry, we're full.";
+            }
+            this.vehicles.push(newVehicle);
+            return "Vehicle added!";
+        }
+    }
